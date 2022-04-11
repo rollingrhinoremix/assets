4e1545cc2cdce7c5edbd4595617007552eb531cf
Joe Cefoli <jcefoli@gmail.com>
2022-04-10 23:42:40 -0400

    Detect locale when adding devel apt sources.list mirrors

--- .sources.sh
+++ .sources.sh
@@ -1,12 +1,14 @@
-echo "deb http://gb.archive.ubuntu.com/ubuntu/ devel main restricted" > /etc/apt/sources.list
+#!/usr/bin/env bash
+countryCode=$(locale | awk -F"[_.]" '/LANG/{print tolower($2)}')
 
 cat >> /etc/apt/sources.list << REPOS
-deb http://gb.archive.ubuntu.com/ubuntu/ devel-updates main restricted
-deb http://gb.archive.ubuntu.com/ubuntu/ devel universe
-deb http://gb.archive.ubuntu.com/ubuntu/ devel-updates universe
-deb http://gb.archive.ubuntu.com/ubuntu/ devel multiverse
-deb http://gb.archive.ubuntu.com/ubuntu/ devel-updates multiverse
-deb http://gb.archive.ubuntu.com/ubuntu/ devel-backports main restricted universe multiverse
+deb http://$countryCode.archive.ubuntu.com/ubuntu/ devel main restricted
+deb http://$countryCode.archive.ubuntu.com/ubuntu/ devel-updates main restricted
+deb http://$countryCode.archive.ubuntu.com/ubuntu/ devel universe
+deb http://$countryCode.archive.ubuntu.com/ubuntu/ devel-updates universe
+deb http://$countryCode.archive.ubuntu.com/ubuntu/ devel multiverse
+deb http://$countryCode.archive.ubuntu.com/ubuntu/ devel-updates multiverse
+deb http://$countryCode.archive.ubuntu.com/ubuntu/ devel-backports main restricted universe multiverse
 deb http://security.ubuntu.com/ubuntu devel-security main restricted
 deb http://security.ubuntu.com/ubuntu devel-security universe
 deb http://security.ubuntu.com/ubuntu devel-security multiverse
