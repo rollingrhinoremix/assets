41a2c4c81e9cebe53aa4247f960458b714a2b4ce
Sourajyoti Basak <wiz28@protonmail.com>
2022-04-19 18:56:54 +0000

    feat(sources): switch to `mirror` protocol

--- .sources.sh
+++ .sources.sh
@@ -1,13 +1,13 @@
 echo "deb http://gb.archive.ubuntu.com/ubuntu/ devel main restricted" > /etc/apt/sources.list
 
 cat >> /etc/apt/sources.list << REPOS
-deb http://gb.archive.ubuntu.com/ubuntu/ devel-updates main restricted
-deb http://gb.archive.ubuntu.com/ubuntu/ devel universe
-deb http://gb.archive.ubuntu.com/ubuntu/ devel-updates universe
-deb http://gb.archive.ubuntu.com/ubuntu/ devel multiverse
-deb http://gb.archive.ubuntu.com/ubuntu/ devel-updates multiverse
-deb http://gb.archive.ubuntu.com/ubuntu/ devel-backports main restricted universe multiverse
-deb http://security.ubuntu.com/ubuntu devel-security main restricted
-deb http://security.ubuntu.com/ubuntu devel-security universe
-deb http://security.ubuntu.com/ubuntu devel-security multiverse
+deb mirror://mirrors.ubuntu.com/mirrors.txt devel-updates main restricted
+deb mirror://mirrors.ubuntu.com/mirrors.txt devel universe
+deb mirror://mirrors.ubuntu.com/mirrors.txt devel-updates universe
+deb mirror://mirrors.ubuntu.com/mirrors.txt devel multiverse
+deb mirror://mirrors.ubuntu.com/mirrors.txt devel-updates multiverse
+deb mirror://mirrors.ubuntu.com/mirrors.txt devel-backports main restricted universe multiverse
+deb mirror://mirrors.ubuntu.com/mirrors.txt devel-security main restricted
+deb mirror://mirrors.ubuntu.com/mirrors.txt devel-security universe
+deb mirror://mirrors.ubuntu.com/mirrors.txt devel-security multiverse
 REPOS
